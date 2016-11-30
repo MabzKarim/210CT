a1ebf1e4ff6ec6f3e8238d5e20023c88f3dc5ce5
MabzKarim <reputationk@gmail.com>
2016-11-30 17:11:25 +0000

    Delete Week 2 - Task 1 (b).py

--- Week 2 - Task 1 (b).py
+++
@@ -1,9 +0,0 @@
-import math
-
-def HIGHEST_PERFECT_SQUARE(r):
-""" This function would find the highest perfect square for any given number"""
-    temp = int(math.sqrt(r))
-    # the sqrt of the given number is rounded down(truncated)
-    hps = temp**2
-    # the rounded down number is squared to find the highest perfect square
-    return hps
