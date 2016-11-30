509006938228451977aa63da129ceb6db95ced4f
MabzKarim <reputationk@gmail.com>
2016-11-30 17:10:43 +0000

    Update and rename Week 2 - Task 1 (a).py to Question 3

--- Week 2 - Task 1 (a).py
+++
@@ -1,8 +0,0 @@
-##Pseudocode for Highest Perfect Square:
-"""This is the pseudocode for the highest perfect square, it will return
-   the highest perfect square for the number the user has chosen"""
-    
-## HIGHEST_PERF_SQUARE(p)
-##  temp <- int sqrt(p)
-##  hps <- temp ^ 2
-##  return hps
